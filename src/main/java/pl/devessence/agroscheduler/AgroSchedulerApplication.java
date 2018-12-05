4c6ba79982274fcd3bc7e46328111bee07fcd74b
amadzia <agnieszkamadzia@onet.pl>
2018-12-05 14:11:55 +0100

    first commit

---
+++ src/main/java/pl/devessence/agroscheduler/AgroSchedulerApplication.java
@@ -0,0 +1,12 @@
+package pl.devessence.agroscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgroSchedulerApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/devessence/agroscheduler/AgroSchedulerApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.devessence.agroscheduler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AgroSchedulerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
